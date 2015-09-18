ee7936576f8999ecdd67411ca4fad81e0ee49c6a
Vaughn Dice <student@epicodus.com>
2015-09-18 10:31:47 -0700

    add Pizzeria object and base specs/functionality

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,9 @@
-function Topping(name) {
+var CURRENCIES = {'JPY': {name: 'yen', symbol: '¥', costSuffix: '00'},
+                  'USD': {name: 'usd', symbol: '$', costSuffix: ' '}};
+
+function Topping(name, price) {
   this.name = name;
-  this.price = 0;
+  this.price = price;
 }
 
 Topping.prototype.setPrice = function(price) {
@@ -9,4 +12,51 @@ Topping.prototype.setPrice = function(price) {
 
 Topping.prototype.getPrice = function() {
   return this.price;
+}
+
+function Pizza(size, cost) {
+  this.size = size;
+  this.cost = cost;
+}
+
+function Pizzeria() {
+  this.currency = CURRENCIES['JPY'];
+  this.sizes = ['7', '11', '15', '19', '23'];
+  this.costs = this.calculateCosts(this.currency);
+  this.pizzas = this.getPizzas();
+}
+
+Pizzeria.prototype.calculateCosts = function(currency) {
+  var costs = {};
+
+  var self = this;
+  this.sizes.forEach(function(size) {
+    costs[size] = Number(size + currency.costSuffix);
+  });
+
+  return costs;
+}
+
+Pizzeria.prototype.getPizzas = function() {
+  var pizzas = {};
+
+  var self = this;
+  this.sizes.forEach(function(size) {
+    pizzas[size] = new Pizza(size, self.costs[size]);
+  });
+
+  return this.pizzas ? this.pizzas : pizzas;
+}
+
+Pizzeria.prototype.changeCurrency = function(currency) {
+  this.costs = this.calculateCosts(currency);
+
+  var self = this;
+  for (var size in this.pizzas) {
+    this.pizzas[size].cost = self.costs[size];
+  }
+}
+
+Pizzeria.prototype.cost = function(size) {
+  return this.costs[size];
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,12 +1,42 @@
 describe('Topping', function() {
-  var testTopping = new Topping('calamari');
+  var testTopping = new Topping('calamari', 100);
 
   it('returns its name', function() {
     expect(testTopping.name).to.equal('calamari');
   });
 
   it('returns its price', function() {
-    testTopping.setPrice(100);
     expect(testTopping.getPrice()).to.equal(100);
   });
+
+  it('returns its altered price', function() {
+    testTopping.setPrice(200);
+    expect(testTopping.getPrice()).to.equal(200);
+  });
 });
+
+describe('Pizza', function() {
+  var testPizza = new Pizza('7', 700);
+
+  it('returns its base price with no toppings', function() {
+    expect(testPizza.cost).to.equal(700);
+  });
+});
+
+describe('Pizzeria', function() {
+  var testPizzeria = new Pizzeria();
+
+  it('returns base pizzas', function() {
+    expect(testPizzeria.getPizzas()['7']).to.eql(new Pizza('7', 700));
+  });
+
+  it('returns cost of a pizza', function() {
+    expect(testPizzeria.cost('7')).to.equal(700);
+  });
+
+  it('returns currency-appropriate costs', function() {
+    expect(testPizzeria.pizzas['7'].cost).to.equal(700);
+    testPizzeria.changeCurrency(CURRENCIES['USD']);
+    expect(testPizzeria.pizzas['7'].cost).to.equal(7);
+  });
+})
